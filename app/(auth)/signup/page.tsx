2a920979a841e6b459508b944ecc3cda23dd84e5
adekunleaishat1 <aishatadekunle877@gmail.com>
2026-02-11 15:11:22 +0100

    typescript

---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,51 @@
+"use client"
+
+import React, {useState} from 'react'
+import { ChangeEvent } from 'react'
+
+const SignupPage = () => {
+    type userdetail = {
+       username:string,
+       email:string,
+       password:string,
+       profilepicture?:string
+    }
+    const [username, setusername] = useState<string>()
+    const [email, setemail] = useState<string>()
+    const [password, setpassword] = useState<string>()
+    const [alluser, setalluser] = useState<Array<userdetail>>([])
+    const [count, setcount] = useState<Array<number>>([])
+  
+    const update = () =>{
+        
+       setusername("4")
+       setemail("false")
+       const user = {
+        username:"shola",
+        email:"shola@gmail.com",
+        password:"sholade"
+       }
+       setalluser([...alluser, user])
+    }
+    const Parameter = (el:number): boolean =>{
+       if (el == 5) {
+          return true
+       }
+       return false
+    }
+     console.log(Parameter(5));
+    const handleinputchange = (e:React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>)  =>{
+      console.log(e.target.value);
+    }
+  return (
+    <div>
+       {username}
+       <button onClick={update}>update</button>
+       <input onChange={handleinputchange} type="text" />
+       <textarea onChange={handleinputchange}></textarea>
+       {/* <button onClick={()=>Parameter(5)}>parameter</button> */}
+    </div>
+  )
+}
+
+export default SignupPage
---
+++ app/(grouproute)/landing/page.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const Landingpage = () => {
+  return (
+    <div >
+      <h1>welcome to your landing page</h1>
+    </div>
+  )
+}
+
+export default Landingpage
